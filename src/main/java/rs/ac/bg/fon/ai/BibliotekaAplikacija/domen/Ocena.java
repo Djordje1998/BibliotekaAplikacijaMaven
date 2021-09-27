2c9dd0bd6644cb115e37ad2b6cbd476769386642
Djordje1998 <djordjen7@gmail.com>
2021-09-27 13:32:48 +0200

    Napisana dokumentacija domenske klase Ocena.

--- src/main/java/rs/ac/bg/fon/ai/BibliotekaAplikacija/domen/Ocena.java
+++ src/main/java/rs/ac/bg/fon/ai/BibliotekaAplikacija/domen/Ocena.java
@@ -8,28 +8,64 @@ package rs.ac.bg.fon.ai.BibliotekaAplikacija.domen;
 import java.sql.ResultSet;
 
 /**
+ * Klasa koja predstavlja ocene komponente za odredjeni test.
+ * Svaka ocena sadrzi referencu ka postojecoj komponenti i stres testu kao i vrednost ocene
  *
  * @author DarkForce
+ * @version 1.0
  */
 public class Ocena implements GenericEntity {
 
+	/**
+	 * Komponenta koja se ocenjuje kao klasa Komponenta.
+	 */
     private Komponenta komponenta;
+    
+    /**
+     * Stress test na osnovu koga se vrsi ocena kao StressTest.
+     */
     private StressTest stressTest;
+    
+    /**
+     * Vrednost ocene kao double vrednost.
+     */
     private double vrednostOcene;
 
+    /**
+     * Konstruktor bez parametara koji inicijalizuje objekat.
+     */
     public Ocena() {
     }
 
+    /**
+     * Konstruktor koji inicijalizuje objekat i postavlja vrednosti za komponentu, test i vrednost ocene.
+     * 
+     * @param komponenta Komponenta koja je ocenjena kao klasa Komponenta.
+     * @param stressTest Stress test na osnovu koga se vrsi ocena kao StressTest.
+     * @param vrednostOcene Vrednost ocene kao double vrednost.
+     */
     public Ocena(Komponenta komponenta, StressTest stressTest, double vrednostOcene) {
         setKomponenta(komponenta);
         setStressTest(stressTest);
         setVrednostOcene(vrednostOcene);
     }
 
+    /**
+     * Vraca vrednost ocene.
+     * 
+     * @return Vrednost ocene kao double.
+     */
     public double getVrednostOcene() {
         return vrednostOcene;
     }
 
+    /**
+     * Postavlja vrednost ocene.
+     * 
+     * @param vrednostOcene Vrednost ocene kao double.
+     * 
+     * @throws lava.lang.RuntimeException Ako je vrednost atributa double vrednost manja od nule.
+     */
     public void setVrednostOcene(double vrednostOcene) {
     	if(vrednostOcene<0) {
     		throw new RuntimeException("Vrednost ocene mora biti pozitivan broj");
@@ -37,10 +73,22 @@ public class Ocena implements GenericEntity {
         this.vrednostOcene = vrednostOcene;
     }
 
+    /**
+     * Vraca komponentu koja se ocenjuje.
+     * 
+     * @return Komponenta koja se ocenjuje kao klasa Komponenta.
+     */
     public Komponenta getKomponenta() {
         return komponenta;
     }
 
+    /**
+     * Postavlja komponentu koja se ocenjuje.
+     * 
+     * @param komponenta Komponenta koja se ocenjuje kao klasa Komponenta.
+     * 
+     * @throws java.lang.NullPointerException Ako je vrednost parametra null.
+     */
     public void setKomponenta(Komponenta komponenta) {
     	if(komponenta==null) {
     		throw new NullPointerException("Komponenta ima null vrednost");
@@ -48,10 +96,22 @@ public class Ocena implements GenericEntity {
         this.komponenta = komponenta;
     }
 
+    /**
+     * Vraca stress test na osnovu koga se vrsi ocena komponente.
+     * 
+     * @return Stress test kao klasa StressTest.
+     */
     public StressTest getStressTest() {
         return stressTest;
     }
 
+    /**
+     * Postavlja vrednost stress testa na osnovu koga se vrsi ocena komponente.
+     * 
+     * @param stressTest Stress test kao klasa StressTest.
+     * 
+     * @throws java.lang.NullPointerException Ako je vrednost parametra null.
+     */
     public void setStressTest(StressTest stressTest) {
     	if(stressTest==null) {
     		throw new NullPointerException("Stress test ima null vrednost");
@@ -59,16 +119,25 @@ public class Ocena implements GenericEntity {
         this.stressTest = stressTest;
     }
 
+    /**
+     * @return Vraca String ime tabele koje se nalazi u bazi podataka i koje odgovara klasi.
+     */
     @Override
     public String getTableName() {
         return "ocena";
     }
 
+    /**
+     * @return Vraca String imena kolona u odgovarajucoj tabeli za dodavanje objekta u bazu.
+     */
     @Override
     public String getColumnNamesForInsert() {
         return "KomponentaID,StressTestID,VrednostOcene";
     }
 
+    /**
+     * @return Vraca String koji predstavlja SQL kod za unos konkretnih vrednosti objekta u bazu.
+     */
     @Override
     public String getInsertValues() {
         StringBuilder sb = new StringBuilder();
@@ -80,30 +149,46 @@ public class Ocena implements GenericEntity {
 
     @Override
     public void setId(int id) {
+    	// nije potrebno jer se primarni kljuc dobija preko Komponente i StressTest-a.
     }
 
+    /**
+     * @return Vraca String koji predstavlja SQL kod koji spaja vise tabela iz baze.
+     */
     @Override
     public String getJoinTables() {
         // ne treba
         return "";
     }
 
+    /**
+     * @return Vraca String koji predstavlja kriterijum pretrage koji se dobija u query-ju od baze.
+     */
     @Override
     public String getLikeCriteria() {
         return "";
     }
 
+    /**
+     * @return Vraca String koji predstavlja kriterijum pretrage postojecih vrednosti u bazi.
+     */
     @Override
     public String getWhereCriteria() {
         return "";
     }
 
+    /**
+     * @return GenericEntity u kome se nalaze vrednosti kao odgovor query-ja iz baze.
+     */
     @Override
     public GenericEntity getResponse(ResultSet rs) throws Exception {
         // ne treba
         return null;
     }
 
+    /**
+     * @return Vraca String koji predstavlja kriterijum brisanja postojecih vrednosti u bazi.
+     */
     @Override
     public String getDeleteCriteria() {
         StringBuilder sb = new StringBuilder();
@@ -112,6 +197,9 @@ public class Ocena implements GenericEntity {
         return sb.toString();
     }
 
+    /**
+     * @return Vraca String koji predstavlja nove vrednosti koje ce biti zamenjene starim koje se vec nalaze u bazi.
+     */
     @Override
     public String getEditValues() {
         StringBuilder sb = new StringBuilder();
@@ -119,6 +207,9 @@ public class Ocena implements GenericEntity {
         return sb.toString();
     }
 
+    /**
+     * @return Vraca String koji predstavlja kriterijum izmene postojecih vrednosti u bazi.
+     */
     @Override
     public String getEditCriteria() {
         StringBuilder sb = new StringBuilder();
